ed8da88db909d1b48ddfa24c80fcf72f3348982e
zach <zekariyasamdu24@gmail.com>
2025-07-26 16:10:02 +0300

    feat: toning the sidebar for netgasha

--- frontend/src/components/composed/app-sidebar.tsx
+++ frontend/src/components/composed/app-sidebar.tsx
@@ -1,41 +1,76 @@
-import { SidebarContent, Sidebar, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+import { SidebarContent, Sidebar, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarHeader, SidebarFooter } from "../ui/sidebar";
+import { Inbox, MapPin, ShieldBan, Gauge, ShieldAlert, BadgeInfo, ChevronDown, ChevronUp, FolderKanban} from "lucide-react"
  
 
 const items = [
   {
-    title: "Home",
+    title: "Blocked IP",
     url: "#",
-    icon: Home,
+    icon: ShieldBan,
   },
   {
-    title: "Inbox",
+    title: "White List",
     url: "#",
     icon: Inbox,
   },
   {
-    title: "Calendar",
+    title: "Rate Limiting",
     url: "#",
-    icon: Calendar,
+    icon: Gauge,
   },
   {
-    title: "Search",
+    title: "Geo Fencing",
     url: "#",
-    icon: Search,
+    icon: MapPin,
+  },
+]
+
+const logs = [
+  {
+    title: "Access Logs",
+    url: "#",
+    icon: BadgeInfo,
   },
   {
-    title: "Settings",
+    title: "Attack Logs",
     url: "#",
-    icon: Settings,
-  },
+    icon: ShieldAlert,
+  }
 ]
 
 export function AppSidebar() {
   return (
    <Sidebar>
+
+    <SidebarHeader>
+    <SidebarMenu>
+      <SidebarMenuItem>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton>
+              Select Workspace
+              <ChevronDown className="ml-auto" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+            <DropdownMenuItem>
+              <span>Acme Inc</span>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <span>Acme Corp.</span>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </SidebarMenuItem>
+    </SidebarMenu>
+  </SidebarHeader>
+
+
+
       <SidebarContent  >
         <SidebarGroup>
-          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupLabel>dashboard</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
@@ -53,7 +88,56 @@ export function AppSidebar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
+
+        <SidebarGroup>
+          <SidebarGroupLabel>Logs</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {logs.map((item) => (
+                
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+                
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
       </SidebarContent>
+
+      <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                    <FolderKanban /> Project
+                    <ChevronUp className="ml-auto" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  side="top"
+                  className="w-[--radix-popper-anchor-width]"
+                >
+                  <DropdownMenuItem>
+                    <span>Account</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <span>Inbox</span>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem>
+                    <span>Setting</span>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
     </Sidebar>
   )
 }
